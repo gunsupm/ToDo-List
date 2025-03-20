dde4d880b0908f014c2e2b6ebfd20a5a7c66d1e3
GunnerSupanut <gunsupm@gmail.com>
2025-03-20 17:07:37 +0700

    update git

--- backend/app.py
+++ backend/app.py
@@ -13,6 +13,8 @@ db = SQLAlchemy(app)
 
 import routes
 
+with app.app_context():
+  db.create_all()
 
 if __name__ == "__main__": 
     app.run(debug=True)
--- backend/routes.py
+++ backend/routes.py
@@ -4,8 +4,33 @@ from models import Task
 
 
 #get all task
-@app.route("/api/task",methods=["GET"])
+@app.route("/api/tasks",methods=["GET"])
 def get_task():
     tasks = Task.query.all()
     result = [task.to_json() for task in tasks ]
     return jsonify(result)
+
+# Create a task
+@app.route("/api/tasks",methods=["POST"])
+def create_task():
+  try:
+    data = request.json
+
+    required_fields = ["name","completed"]
+    for field in required_fields:
+      if field not in data or not data.get(field):
+        return jsonify({"error":f'Missing required field: {field}'}), 400
+
+    name = data.get("name")
+    completed = data.get("completed")
+
+    new_friend = Friend(name=name, completed=completed)
+
+    db.session.add(new_friend) 
+    db.session.commit()
+
+    return jsonify(new_friend.to_json()), 201
+    
+  except Exception as e:
+    db.session.rollback()
+    return jsonify({"error":str(e)}), 500
